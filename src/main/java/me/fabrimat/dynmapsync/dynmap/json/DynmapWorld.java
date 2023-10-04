d6c6a7f669c519c33ebebc95d52e362e0f1dd962
Fabrizio La Rosa <lr.fabrizio@gmail.com>
2023-10-04 17:14:06 +0200

    Finish Dynmap World object

--- src/main/java/me/fabrimat/dynmapsync/dynmap/json/DynmapWorld.java
+++ src/main/java/me/fabrimat/dynmapsync/dynmap/json/DynmapWorld.java
@@ -1,16 +1,18 @@
 package me.fabrimat.dynmapsync.dynmap.json;
 
 import com.google.gson.annotations.SerializedName;
+import me.fabrimat.dynmapsync.dynmap.json.update.Update;
 
 public class DynmapWorld {
     @SerializedName("currentcount")
     private int currentCount;
     private boolean hasStorm;
-    private DynmapPlayer[] players;
+    private DynmapPlayer[] players = {};
     private boolean isThundering;
     @SerializedName("servertime")
     private long serverTime;
     @SerializedName("configHash")
     private long configHash;
+    private Update[] updates = {};
 
 }
