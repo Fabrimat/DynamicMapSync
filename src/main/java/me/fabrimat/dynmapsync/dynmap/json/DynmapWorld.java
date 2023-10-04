a017a023a4d8fc36ab177288ae45ebaf70299a63
Fabrimat <lr.fabrizio@gmail.com>
2023-10-04 19:28:19 +0200

    Add getters and setters in DynmapWorld

--- src/main/java/me/fabrimat/dynmapsync/dynmap/json/DynmapWorld.java
+++ src/main/java/me/fabrimat/dynmapsync/dynmap/json/DynmapWorld.java
@@ -15,4 +15,59 @@ public class DynmapWorld {
     private long configHash;
     private Update[] updates = {};
 
+    public int getCurrentCount() {
+        return currentCount;
+    }
+
+    public void setCurrentCount(int currentCount) {
+        this.currentCount = currentCount;
+    }
+
+    public boolean isHasStorm() {
+        return hasStorm;
+    }
+
+    public void setHasStorm(boolean hasStorm) {
+        this.hasStorm = hasStorm;
+    }
+
+    public DynmapPlayer[] getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(DynmapPlayer[] players) {
+        this.players = players;
+    }
+
+    public boolean isThundering() {
+        return isThundering;
+    }
+
+    public void setThundering(boolean thundering) {
+        isThundering = thundering;
+    }
+
+    public long getServerTime() {
+        return serverTime;
+    }
+
+    public void setServerTime(long serverTime) {
+        this.serverTime = serverTime;
+    }
+
+    public long getConfigHash() {
+        return configHash;
+    }
+
+    public void setConfigHash(long configHash) {
+        this.configHash = configHash;
+    }
+
+    public Update[] getUpdates() {
+        return updates;
+    }
+
+    public void setUpdates(Update[] updates) {
+        this.updates = updates;
+    }
 }
